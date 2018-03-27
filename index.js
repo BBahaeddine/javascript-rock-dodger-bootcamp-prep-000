2de5cead3d3810bf7ec41cde35c477eb10781340
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-03-27 23:30:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -111,7 +111,8 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval);
   for(let i = 0; i < ROCKS.length; i++) {
-    GAME.removeChild(ROCKS[i]);
+    let rock = createRock(ROCK[i]);
+    GAME.remove(rock);
   }
   window.removeEventListener('keydown', moveDodger);
   alert('YOU LOSE!');
